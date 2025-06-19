ae286df5c2ffa980fdd5b901ae79e8f450a0cbee
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2025-06-19 00:36:46 -0300

    nova aula, exercicio simples mas me fez pensar no complicado inves do simples

---
+++ python_basics/lesson47.py
@@ -0,0 +1,36 @@
+
+trys_user = ''
+secret_word = 'python'
+count = 0
+
+while True:
+    
+    input_user = input('Digite uma palavra: ').lower()
+    count += 1
+
+    
+    if len(input_user) > 1:
+        print('Digite apenas uma palavra')
+        continue
+    
+    
+    if input_user in secret_word:
+        trys_user += input_user
+    
+    
+    revelead_word = '' 
+    for actual_phrase in secret_word:
+        if actual_phrase in trys_user:
+            revelead_word += actual_phrase
+        else:
+            revelead_word += '*'
+    print(revelead_word)
+    
+    
+    if revelead_word == secret_word:
+        print('Parabens voce descobriu!')
+        print('A Palavra secreta era:', secret_word)
+        print('Tentativas:', count)
+        trys_user = ''
+        revelead_word = ''
+        continue
